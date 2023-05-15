ff0d97bed0eb94cfcde1063e79687e4a590bc0e9
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-15 14:56:45 -0500

    demo arrow function

---
+++ demo-arrow-functions.js
@@ -0,0 +1,16 @@
+const numbers = [3, 5, 7, 9];
+
+const sumReducer = (a, b) => a + b;
+
+const sum = numbers.reduce(sumReducer, 0);
+console.log('sum', sum);
+
+const names = ['Adan', 'Cecelia', 'Chauni', 'Dillon', 'Noah'];
+
+// These two functions are equivalent
+function funcFindC(name) {
+  return name.startsWith('C');
+}
+const arrowFindC = (name) => name.startsWith('C');
+
+const firstC = names.find(arrowFindC);
