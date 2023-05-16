f20018d83c72c91579a809e72df739dfe5e8350f
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-16 12:02:42 -0500

    more comments

--- demo-md-arrays.js
+++ demo-md-arrays.js
@@ -16,11 +16,19 @@ let teamMembers = [
 teamMembers[2][2]; // Mollye
 teamMembers[2][3]; // Ranse
 
-// teamMembers.forEach((team) => {
-//   console.log('team ---');
-//   team.forEach((player) => console.log(player));
-// });
+// You can loop through multi-dimensional arrays using
+// nested forEach loops. I find this to be the most
+// intuitive, but others may disagree.
+teamMembers.forEach((team) => {
+  console.log('team ---');
+  team.forEach((player) => console.log(player));
+});
 
+// You can also loop through them using plain old for
+// loops. I find this more error-prone, because you have
+// to keep two separate indexes (i and j in this case), and
+// it's easy to use the wrong one and difficult to spot
+// when you do.
 for (let i = 0; i < teamMembers.length; i++) {
   console.log('team ---');
   for (let j = 0; j < teamMembers[i].length; j++) {
