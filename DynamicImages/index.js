bd7b1c85b5edd9f119c054e9fad1d85dbe3152c8
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-17 15:06:21 -0500

    almost finished. One bug to fix in clear button

--- DynamicImages/index.js
+++ DynamicImages/index.js
@@ -1,7 +1,40 @@
 const imageFiles = [
-  { name: './images/black-bear.jpg', description: 'Black Bear' },
-  { name: './images/grizzly-bear.jpg', description: 'Grizzly Bear' },
-  { name: './images/polar-bear.jpg', description: 'Poloar Bear' },
-  { name: './images/sun-bear.jpg', description: 'Sun Bear' },
-  { name: './images/teddy-bear.jpg', description: 'Teddy Bear' }
+  { path: './images/black-bear.jpg', description: 'Black Bear' },
+  { path: './images/grizzly-bear.jpg', description: 'Grizzly Bear' },
+  { path: './images/polar-bear.jpg', description: 'Polar Bear' },
+  { path: './images/sun-bear.jpg', description: 'Sun Bear' },
+  { path: './images/teddy-bear.jpg', description: 'Teddy Bear' }
 ];
+
+const selectEl = document.getElementById('imageSelector');
+const addImageBtn = document.getElementById('addImage');
+const imagesDiv = document.getElementById('images');
+const clearBtn = document.getElementById('clearImages');
+
+for (let i = 0; i < imageFiles.length; i++) {
+  const optionEl = document.createElement('option');
+  optionEl.textContent = imageFiles[i].description;
+  optionEl.value = imageFiles[i].path;
+  selectEl.appendChild(optionEl);
+}
+
+addImageBtn.addEventListener('click', () => {
+  const selectedImageValue = selectEl.value;
+  if (selectedImageValue === '') {
+    return;
+  }
+
+  // find method - look over workbook 5, page 1-7 and beyond
+  const selectedImageObj = imageFiles.find((img) => img.path === selectedImageValue);
+  const imgEl = document.createElement('img');
+  imgEl.src = selectedImageObj.path;
+  imgEl.alt = selectedImageObj.description;
+  imagesDiv.appendChild(imgEl);
+});
+
+clearBtn.addEventListener('click', () => {
+  const images = document.getElementsByTagName('img');
+  Array.from(images).forEach((img) => {
+    imagesDiv.removeChild(img);
+  });
+});
