8eff7314fa128dcda649263be508961d95aeef01
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-17 13:09:46 -0500

    DynamicImages starting point

---
+++ DynamicImages/index.js
@@ -0,0 +1,7 @@
+const imageFiles = [
+  { name: './images/black-bear.jpg', description: 'Black Bear' },
+  { name: './images/grizzly-bear.jpg', description: 'Grizzly Bear' },
+  { name: './images/polar-bear.jpg', description: 'Poloar Bear' },
+  { name: './images/sun-bear.jpg', description: 'Sun Bear' },
+  { name: './images/teddy-bear.jpg', description: 'Teddy Bear' }
+];
